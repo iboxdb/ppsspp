b73c51a4a2b50b5430ec87e508ce3882739ddbe3
User <User@User>
2017-04-17 10:31:01 -0700

    tvshow java

---
+++ tvshow/tvshow_java/src/tvshow/FXMLDocumentController.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tvshow;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Root
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Label label;
+
+    private BufferedInputStream file;
+    private WritableImage image;
+
+    int s_width;
+    int s_height;
+    Stage stage;
+
+    @FXML
+    private AnchorPane ap;
+    @FXML
+    private Canvas canvas;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //   ap.get
+    }
+
+    void load(Stage stage) throws Exception {
+        this.stage = stage;
+        String path = "C:\\PSP\\ppsspp\\memstick\\PSP\\VIDEO\\fd1492440010-38130.tvi";
+        file = new BufferedInputStream(new FileInputStream(path), 8 * 1024 * 1024);
+
+        byte version = (byte) file.read();
+        byte intsize = (byte) file.read();
+
+        s_width = file.read() & 0xFF;
+        s_width |= (file.read() & 0xFF) << 8;
+
+        file.read();
+        file.read();
+
+        s_height = file.read() & 0xFF;
+        s_height |= (file.read() & 0xFF) << 8;
+
+        file.read();
+        file.read();
+
+        file.read();
+        file.read();
+        file.read();
+        file.read();
+
+        stage.setWidth(s_width + 50);
+        stage.setHeight(s_height + 50);
+
+        canvas.setWidth(stage.getWidth());
+        canvas.setHeight(stage.getHeight());
+        canvas.getGraphicsContext2D().fillText("AAABBB", 50, 50);
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                try {
+                    next();
+                } catch (Exception ex) {
+                    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }.start();
+
+    }
+
+    private void next() throws Exception {
+        if (file.available() > 0) {
+            int ms = file.read() & 0xFF;
+            ms |= (file.read() & 0xFF) << 8;
+            ms |= (file.read() & 0xFF) << 16;
+            ms |= (file.read() & 0xFF) << 24;
+
+            stage.setTitle(Integer.toString(ms));
+
+            image = new WritableImage(s_width, s_height);
+            PixelWriter writer = image.getPixelWriter();
+
+            for (int h = 0; h < s_height; h++) {
+                for (int w = 0; w < s_width; w++) {
+                    /*
+                    int rgb = file.read() & 0xFF;
+                    rgb |= (file.read() & 0xFF) << 8;
+                    rgb |= (file.read() & 0xFF) << 16;
+                    rgb |= (0xFF) << 24;
+                    writer.setArgb(w, h, rgb);
+                     */
+                    writer.setColor(w, h, Color.rgb(file.read() & 0xFF, file.read() & 0xFF, file.read() & 0xFF));
+                }
+            }
+        } else {
+            image = new WritableImage(s_width, s_height);
+            PixelWriter writer = image.getPixelWriter();
+
+            for (int h = 0; h < s_height; h++) {
+                for (int w = 0; w < s_width; w++) {
+                    int rgb = 0xAAAAAAAA;
+                    writer.setArgb(w, h, rgb);
+                }
+            }
+            stage.setTitle("END.");
+        }
+        canvas.getGraphicsContext2D().drawImage(image, 0, 0);
+    }
+
+}
